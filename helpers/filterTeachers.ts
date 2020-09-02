a40598e5c19b2250e0ad3d9d70472bd597ff90a2
Operel <oriperelman@gmail.com>
2020-09-02 09:36:05 +0300

    Some doc comments

--- helpers/filterTeachers.ts
+++ helpers/filterTeachers.ts
@@ -6,6 +6,11 @@ import { RawClass } from '../types';
 
 const rawTeachersData = fs.readFileSync(path.join(__dirname, '../sources/teachers.csv'));
 
+/**
+ * Create an array of teachers' names
+ * 
+ * @description Receive data from CSV and format teachers' names to go into the array. 
+ */
 const createTeachersList = async (): Promise<string[]> => {
   const teachersData = await neatCsv(rawTeachersData);
   return teachersData.map((teacher: any) => {
@@ -13,6 +18,12 @@ const createTeachersList = async (): Promise<string[]> => {
   });
 }
 
+/**
+ * Filter classes 
+ * 
+ * @description Filters the classes with teachers in the array created by createTeachersList()
+ * @param classData Array of all classes from the HTML table
+ */
 export const filterClassData = async (classData: RawClass[]): Promise<RawClass[]> => {
   const ls: string[] = await createTeachersList();
   return classData.filter((c: RawClass) => {
